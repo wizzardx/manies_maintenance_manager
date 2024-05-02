2a7571202ffe4dad5e2e6255f697644c091e4da1
David <wizzardx@gmail.com>
2024-05-02 19:56:00 +0200

    First FT specced out in comments

--- manies_maintenance_manager/functional_tests/tests.py
+++ manies_maintenance_manager/functional_tests/tests.py
@@ -18,7 +18,7 @@ class CustomStaticLiveServerTestCase(StaticLiveServerTestCase):
         return f"http://django:{self.server_thread.port}"
 
 
-class MySeleniumTests(CustomStaticLiveServerTestCase):
+class ExistingAgentCreatesMaintenanceJobTest(CustomStaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -34,6 +34,62 @@ class MySeleniumTests(CustomStaticLiveServerTestCase):
         cls.browser.quit()
         super().tearDownClass()
 
-    def test_example(self):
-        self.browser.get(f"{self.live_server_url}/some_page")
-        # Add assertions or interactions here
+    def test_existing_agent_user_can_login_and_create_a_new_maintenance_job_and_logout(
+        self,
+    ):
+        # One of Manies property manager contacts, Bob, an `Agent`, has heard of a cool
+        # new webpage that's for helping to coordinate Manies Maintenance jobs with him.
+        # He goes to check out its homepage
+        self.browser.get(f"{self.live_server_url}")
+
+        # He notices the page title and the navbar mention Manies Maintenance Manager
+        self.assertIn("Manies Maintenance Manager", self.browser.title)
+
+        # He sees the Sign In button in the navbar
+        self.fail("Finish the test!")
+
+        # He clicks on the Sign In button
+
+        # This sends him to the Sign In page, where he notices that the page title and
+        # the header mention Sign In
+
+        # He types "bob" into the Username field
+
+        # He types "bob" into the Password field
+
+        # He clicks the "Sign In" button
+
+        # This sends him to the "Maintenance Jobs" page, where he notices that the page
+        # title and the header mention Jobs
+
+        # He notices a "Create Maintenance Job" link
+
+        # He clicks on the "Create Maintenance Job" link
+
+        # This sends him to the "Create Maintenance Job" page, where he notices that
+        # the page title and the header mention Create Maintenance Job
+
+        # He notices entry fields "Date", "Address Details", "GPS Link", and "Quote
+        # Request Details"
+
+        # He types 2021-01-01 into the "Date" field
+
+        # He types "Department of Home Affairs Bellville" into the "Address Details"
+        # field
+
+        # He copy-pastes "https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7" into the
+        # "GPS Link" field
+
+        # He types "Please fix the leaky faucet in the staff bathroom" into the
+        # "Quote Request Details" field
+
+        # He notices and clicks on a "Submit" button
+
+        # This sends him back to the "Maintenance Jobs" page, where he notices that the
+        # page title and the header mention Maintenance Jobs like before.
+
+        # He notices that the new Maintenance Job is listed on the web page.
+
+        # He clicks on the Sign Out button
+
+        # Satisfied, he goes back to sleep
