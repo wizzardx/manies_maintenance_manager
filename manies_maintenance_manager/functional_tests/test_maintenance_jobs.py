283af4b720d75091f8a61194d694b6dbae008cc5
David <wizzardx@gmail.com>
2024-05-03 21:55:50 +0200

    Use Pytests live_server fixture instead of Django's StaticLiveTestServer

--- manies_maintenance_manager/functional_tests/test_maintenance_jobs.py
+++ manies_maintenance_manager/functional_tests/test_maintenance_jobs.py
@@ -1,107 +1,87 @@
 import pytest
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-class CustomStaticLiveServerTestCase(StaticLiveServerTestCase):
-    # Bind to all interfaces. This is so that in our dev environment, Chrome
-    # running under Selenium in another Docker container, can connect to
-    # our container. This is a bit of an ugly workaround to the default
-    # `StaticLiveServerTestCase` behavior.
-    host = "0.0.0.0"  # noqa: S104
-
-    @property
-    def live_server_url(self):
-        """
-        Returns the live server URL with the internal Docker service name.
-        """
-        return f"http://django:{self.server_thread.port}"
-
-
-@pytest.fixture(scope="class")
-def live_server(request):
-    # Set up the custom live server
-    live_server = CustomStaticLiveServerTestCase()
-    live_server.setUpClass()
-    request.cls.live_server = live_server
-
-    yield live_server
-
-    live_server.tearDownClass()
-
-
-@pytest.fixture(scope="class")
-def browser(request):
+@pytest.fixture()
+def browser():
     options = Options()
     # You can add more options here if needed
     driver = webdriver.Remote(
         command_executor="http://chrome:4444/wd/hub",
         options=options,
     )
-    request.cls.browser = driver
 
     yield driver
 
     driver.quit()
 
 
-@pytest.mark.usefixtures("live_server", "browser")
-class TestExistingAgentCreatesMaintenanceJob:
-    def test_existing_agent_user_can_login_and_create_a_new_maintenance_job_and_logout(
-        self,
-    ):
-        # Use the live_server_url from the modified fixture
-        self.browser.get(self.live_server.live_server_url)
+@pytest.fixture()
+def live_server_url(live_server):
+    """
+    Modify the live_server URL to use 'django' instead of '0.0.0.0', which supports
+    Docker inter-container communication when testing.
+    """
+    return live_server.url.replace("0.0.0.0", "django")  # noqa: S104
 
-        # Continue with the test steps
-        assert "Manies Maintenance Manager" in self.browser.title
 
-        # He sees the Sign In button in the navbar
-        pytest.fail("Finish the test!")
+@pytest.mark.django_db()
+def test_existing_agent_user_can_login_and_create_a_new_maintenance_job_and_logout(
+    browser,
+    live_server_url,
+):
+    # Use the live_server_url from the live_server fixture
+    browser.get(live_server_url)
 
-        # He clicks on the Sign In button
+    # Continue with the test steps
+    assert "Manies Maintenance Manager" in browser.title
 
-        # This sends him to the Sign In page, where he notices that the page title and
-        # the header mention Sign In
+    # He sees the Sign In button in the navbar
+    pytest.fail("Finish the test!")
 
-        # He types "bob" into the Username field
+    # He clicks on the Sign In button
 
-        # He types "secret" into the Password field
+    # This sends him to the Sign In page, where he notices that the page title and
+    # the header mention Sign In
 
-        # He clicks the "Sign In" button
+    # He types "bob" into the Username field
 
-        # This sends him to the "Maintenance Jobs" page, where he notices that the page
-        # title and the header mention Jobs
+    # He types "secret" into the Password field
 
-        # He notices a "Create Maintenance Job" link
+    # He clicks the "Sign In" button
 
-        # He clicks on the "Create Maintenance Job" link
+    # This sends him to the "Maintenance Jobs" page, where he notices that the page
+    # title and the header mention Jobs
 
-        # This sends him to the "Create Maintenance Job" page, where he notices that
-        # the page title and the header mention Create Maintenance Job
+    # He notices a "Create Maintenance Job" link
 
-        # He notices entry fields "Date", "Address Details", "GPS Link", and "Quote
-        # Request Details"
+    # He clicks on the "Create Maintenance Job" link
 
-        # He types 2021-01-01 into the "Date" field
+    # This sends him to the "Create Maintenance Job" page, where he notices that
+    # the page title and the header mention Create Maintenance Job
 
-        # He types "Department of Home Affairs Bellville" into the "Address Details"
-        # field
+    # He notices entry fields "Date", "Address Details", "GPS Link", and "Quote
+    # Request Details"
 
-        # He copy-pastes "https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7" into the
-        # "GPS Link" field
+    # He types 2021-01-01 into the "Date" field
 
-        # He types "Please fix the leaky faucet in the staff bathroom" into the
-        # "Quote Request Details" field
+    # He types "Department of Home Affairs Bellville" into the "Address Details"
+    # field
 
-        # He notices and clicks on a "Submit" button
+    # He copy-pastes "https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7" into the
+    # "GPS Link" field
 
-        # This sends him back to the "Maintenance Jobs" page, where he notices that the
-        # page title and the header mention Maintenance Jobs like before.
+    # He types "Please fix the leaky faucet in the staff bathroom" into the
+    # "Quote Request Details" field
 
-        # He notices that the new Maintenance Job is listed on the web page.
+    # He notices and clicks on a "Submit" button
 
-        # He clicks on the Sign Out button
+    # This sends him back to the "Maintenance Jobs" page, where he notices that the
+    # page title and the header mention Maintenance Jobs like before.
 
-        # Satisfied, he goes back to sleep
+    # He notices that the new Maintenance Job is listed on the web page.
+
+    # He clicks on the Sign Out button
+
+    # Satisfied, he goes back to sleep
