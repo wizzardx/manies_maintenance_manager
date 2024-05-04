0d9dd45bd6fa501454d7c7b3bb1ec1f6480de1ea
David <wizzardx@gmail.com>
2024-05-04 21:04:58 +0200

    Functional test now checks we can input the first section of a Maintenance Job

--- manies_maintenance_manager/functional_tests/test_maintenance_jobs.py
+++ manies_maintenance_manager/functional_tests/test_maintenance_jobs.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture()
@@ -38,50 +39,105 @@ def test_existing_agent_user_can_login_and_create_a_new_maintenance_job_and_logo
     assert "Manies Maintenance Manager" in browser.title
 
     # He sees the Sign In button in the navbar
-    pytest.fail("Finish the test!")
+    sign_in_button = browser.find_element(By.LINK_TEXT, "Sign In")
 
     # He clicks on the Sign In button
+    sign_in_button.click()
 
     # This sends him to the Sign In page, where he notices that the page title and
     # the header mention Sign In
+    assert "Sign In" in browser.title
 
     # He types "bob" into the Username field
+    username_field = browser.find_element(By.ID, "id_login")
+    username_field.send_keys("bob")
 
     # He types "secret" into the Password field
+    password_field = browser.find_element(By.ID, "id_password")
+    password_field.send_keys("password")
 
     # He clicks the "Sign In" button
+    sign_in_button = browser.find_element(By.CLASS_NAME, "btn-primary")
+    sign_in_button.click()
 
     # This sends him to the "Maintenance Jobs" page, where he notices that the page
     # title and the header mention Jobs
+    assert "Maintenance Jobs" in browser.title
+    assert "Maintenance Jobs" in browser.find_element(By.TAG_NAME, "h1").text
 
     # He notices a "Create Maintenance Job" link
+    create_maintenance_job_link = browser.find_element(
+        By.LINK_TEXT,
+        "Create Maintenance Job",
+    )
 
     # He clicks on the "Create Maintenance Job" link
+    create_maintenance_job_link.click()
 
     # This sends him to the "Create Maintenance Job" page, where he notices that
     # the page title and the header mention Create Maintenance Job
+    assert "Create Maintenance Job" in browser.title
+    assert "Create Maintenance Job" in browser.find_element(By.TAG_NAME, "h1").text
 
     # He notices entry fields "Date", "Address Details", "GPS Link", and "Quote
-    # Request Details"
+    # Request Details", as well as a "Submit" button
+    date_field = browser.find_element(By.ID, "id_date")
+    address_details_field = browser.find_element(By.ID, "id_address_details")
+    gps_link_field = browser.find_element(By.ID, "id_gps_link")
+    quote_request_details_field = browser.find_element(
+        By.ID,
+        "id_quote_request_details",
+    )
+    submit_button = browser.find_element(By.CLASS_NAME, "btn-primary")
 
     # He types 2021-01-01 into the "Date" field
+    date_field.send_keys("2021-01-01")
 
     # He types "Department of Home Affairs Bellville" into the "Address Details"
     # field
+    address_details_field.send_keys("Department of Home Affairs Bellville")
 
     # He copy-pastes "https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7" into the
     # "GPS Link" field
+    gps_link_field.send_keys("https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7")
 
     # He types "Please fix the leaky faucet in the staff bathroom" into the
     # "Quote Request Details" field
+    quote_request_details_field.send_keys(
+        "Please fix the leaky faucet in the staff bathroom",
+    )
 
-    # He notices and clicks on a "Submit" button
+    # He clicks the Submit button
+    submit_button.click()
 
     # This sends him back to the "Maintenance Jobs" page, where he notices that the
     # page title and the header mention Maintenance Jobs like before.
+    assert "Maintenance Jobs" in browser.title
+    assert "Maintenance Jobs" in browser.find_element(By.TAG_NAME, "h1").text
 
-    # He notices that the new Maintenance Job is listed on the web page.
+    # He notices that the new Maintenance Job is listed on the web page in a table
+    table = browser.find_element(By.ID, "id_list_table")
+    rows = table.find_elements(By.TAG_NAME, "tr")
+
+    ## There should be only one row:
+    assert len(rows) == 1
+    row = rows[0]
+
+    ## Grab the cell text contents from the row
+    cell_texts = [cell.text for cell in row.find_elements(By.TAG_NAME, "td")]
+
+    # Make sure the cell text contents match the expected values. We also
+    assert cell_texts == [
+        "1",  # This is for the row number, automatically added by the system.
+        "2021-01-01",
+        "Department of Home Affairs Bellville",
+        "https://maps.app.goo.gl/mXfDGVfn1dhZDxJj7",
+        "Please fix the leaky faucet in the staff bathroom",
+    ]
 
     # He clicks on the Sign Out button
+    sign_out_button = browser.find_element(By.LINK_TEXT, "Sign Out")
+    sign_out_button.click()
 
     # Satisfied, he goes back to sleep
+    pytest.fail("Finish the test!")
