16d3f6be1b4982e270312a293f47fb3fabb155d2
David <wizzardx@gmail.com>
2024-05-04 21:50:41 +0200

    Refactor home page view to use a template

--- manies_maintenance_manager/jobs/test_jobs.py
+++ manies_maintenance_manager/jobs/test_jobs.py
@@ -1,12 +1,12 @@
-from django.http import HttpRequest
-
-from manies_maintenance_manager.jobs.views import home_page
+import pytest
 
 
-def test_home_page_returns_correct_html():
-    request = HttpRequest()
-    response = home_page(request)
-    html = response.content.decode("utf8")
-    assert "<title>Manies Maintenance Manager</title>" in html
-    assert html.startswith("<html>")
-    assert html.endswith("</html>")
+@pytest.mark.django_db()
+def test_home_page_returns_correct_html(client):
+    response = client.get("/")
+    response_text = response.content.decode()
+    assert "<title>Manies Maintenance Manager</title>" in response_text
+    assert '<html lang="en">' in response_text
+    assert "</html>" in response_text
+    # Check the first template if multiple are used
+    assert response.template_name[0] == "pages/home.html"
--- manies_maintenance_manager/jobs/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-# Create your views here.
-def home_page(request):
-    return HttpResponse("<html><title>Manies Maintenance Manager</title></html>")
