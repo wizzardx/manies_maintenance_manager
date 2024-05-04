74dd5cdd8fcdd64b0e2eb0493ea8ec120334809a
David <wizzardx@gmail.com>
2024-05-04 17:04:49 +0200

    Add app for jobs, with deliberately failing unit test

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "manies_maintenance_manager.users",
     # Your stuff: custom apps go here
+    "manies_maintenance_manager.jobs",  # Maintenance Jobs
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ manies_maintenance_manager/jobs/test_jobs.py
@@ -0,0 +1,2 @@
+def test_bad_maths():
+    assert 1 + 1 == 3
