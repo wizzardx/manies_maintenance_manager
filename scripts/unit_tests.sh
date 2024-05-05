fde4ef05653253530a7b2b3840c889dc5698d230
David <wizzardx@gmail.com>
2024-05-06 00:25:16 +0200

    Update options used in pytest for unit tests

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker compose -f local.yml run --rm \
-    django pytest manies_maintenance_manager/jobs --ff --maxfail=1 --showlocals
+    django pytest manies_maintenance_manager/jobs --ff --maxfail=1 --showlocals -s
