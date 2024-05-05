2ff21800e61dfbb2de236c75b1a860860ae016d1
David <wizzardx@gmail.com>
2024-05-05 19:13:53 +0200

    Update pytest command-line options

--- scripts/all_tests.sh
+++ scripts/all_tests.sh
@@ -6,7 +6,7 @@ echo "Type checks..."
 docker compose -f local.yml exec django mypy manies_maintenance_manager
 
 echo "Unit and functional tests, with coverage..."
-docker compose -f local.yml exec django coverage run -m pytest
+docker compose -f local.yml exec django coverage run -m pytest --showlocals
 
 echo "Coverage report..."
 docker compose -f local.yml exec django coverage report
--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-docker compose -f local.yml run --rm django pytest manies_maintenance_manager/jobs
+docker compose -f local.yml run --rm \
+    django pytest manies_maintenance_manager/jobs --ff --maxfail=1 --showlocals
