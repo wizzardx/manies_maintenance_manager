286c53642bf73cf2b675e7919b572129043b8f8e
David <wizzardx@gmail.com>
2024-05-06 21:20:47 +0200

    Workaround for something weird happening durin my test scripts

--- scripts/build_and_start_containers.sh
+++ scripts/build_and_start_containers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
+
+# Remove a directory that can cause some problems with building the image:
+if [ -e .ipython/profile_default ]; then
+    sudo rm -rvf .ipython/profile_default
+fi
+
 docker compose -f local.yml up --build
