2ecd08b755568110ab49dfb81b5a99c4f439a84c
David <wizzardx@gmail.com>
2024-05-08 12:29:59 +0200

    Remove another 'typechecked' reference

--- marnies_maintenance_manager/users/context_processors.py
+++ marnies_maintenance_manager/users/context_processors.py
@@ -2,10 +2,8 @@
 
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
-from typeguard import typechecked
 
 
-@typechecked
 def allauth_settings(request: WSGIRequest) -> dict[str, bool]:
     """Expose some settings from django-allauth in templates."""
     return {
