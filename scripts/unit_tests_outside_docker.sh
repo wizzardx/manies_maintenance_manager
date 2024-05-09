8d24ccd6d7ffd0e9c446a0ea412d9da99742a770
David <wizzardx@gmail.com>
2024-05-09 09:58:59 +0200

    Unit testing script now also responds to changes in requirements/base.txt

--- scripts/unit_tests_outside_docker.sh
+++ scripts/unit_tests_outside_docker.sh
@@ -19,15 +19,20 @@ source .venv/bin/activate
 
 # Get md5sum of requirements/local.txt, and use that to determine if we need to
 # run pip install.
-TXT_MD5=$(md5sum requirements/local.txt | awk '{print $1}')
-ALREADY_INSTALLED_MARKER_FILE=".venv/.already_installed_${TXT_MD5}"
+LOCAL_TXT_MD5=$(md5sum requirements/local.txt | awk '{print $1}')
+LOCAL_ALREADY_INSTALLED_MARKER_FILE=".venv/.local_already_installed_${LOCAL_TXT_MD5}"
+
+# Do the same for the requirements/base.txt file:
+BASE_TXT_MD5=$(md5sum requirements/base.txt | awk '{print $1}')
+BASE_ALREADY_INSTALLED_MARKER_FILE=".venv/.base_already_installed_${BASE_TXT_MD5}"
 
 # Only run Pip to install software if this is a new venv (the marker file does not
-# exist), or the user made changes to the "local.txt" file (the marker file md5sum
-# marker is now out of date).
-if [ !  -f $ALREADY_INSTALLED_MARKER_FILE ]; then
+# exist), or the user made changes to the "local.txt" or "base.txt" file (the marker
+# file md5sum markers are now out of date).
+if [[ ! -f $LOCAL_ALREADY_INSTALLED_MARKER_FILE || ! -f $BASE_ALREADY_INSTALLED_MARKER_FILE ]]; then
     python -m pip install -r requirements/local.txt
-    touch $ALREADY_INSTALLED_MARKER_FILE
+    touch $LOCAL_ALREADY_INSTALLED_MARKER_FILE
+    touch $BASE_ALREADY_INSTALLED_MARKER_FILE
 fi
 
 # Setup needed environment variables
