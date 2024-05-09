3822b44ee2de821836e4e15ee7f83fe3d7d06fd5
David <wizzardx@gmail.com>
2024-05-09 14:38:39 +0200

    Make sure that we get both html and terminal coverage reports, even if one of them fails

--- scripts/all_tests.sh
+++ scripts/all_tests.sh
@@ -14,10 +14,20 @@ echo "Unit and functional tests (under docker), with coverage..."
 docker compose -f local.yml exec django coverage run --rcfile=.coveragerc -m pytest --showlocals
 
 echo "Coverage report (console)..."
-docker compose -f local.yml exec django coverage report --rcfile=.coveragerc
+# Run both coverage reports, even if one of them fails, before returning with an exit.
+# This is so that we can have both type of output report (terminal, and HTML).
+COVERAGE_ERROR=0
+docker compose -f local.yml exec django coverage report --rcfile=.coveragerc || COVERAGE_ERROR=1
 
 echo "Coverage report (html)..."
-docker compose -f local.yml exec django coverage html --rcfile=.coveragerc
+docker compose -f local.yml exec django coverage html --rcfile=.coveragerc || COVERAGE_ERROR=1
+
+# Stop now if there was an error (eg, not enough coverage %) returned by one of the
+# coverage report runs
+if [ "$COVERAGE_ERROR" != "0" ]; then
+    echo "Error running coverage"
+    exit 1
+fi
 
 echo "Running pre-commit checks..."
 pre-commit run --all-files
