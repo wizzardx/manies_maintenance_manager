4b61207df387bf57234b409ef69ef0f8e3ca296b
David <wizzardx@gmail.com>
2024-05-20 16:35:26 +0200

    Jump Desktop focus back to test runner script at end.

--- scripts/all_tests_under_pycharm.sh
+++ scripts/all_tests_under_pycharm.sh
@@ -4,6 +4,9 @@ set -e
 # Extra logic for scripts/all_scripts.sh, when we run the script under PyCharm's
 # terminal
 
+# Capture the window ID of the terminal where the script is running
+TERMINAL_WIN_ID=$(xdotool getactivewindow)
+
 # Run the script itself
 RETCODE=0
 
@@ -22,6 +25,10 @@ else
     kdialog --msgbox "Tests done - FAILURE"
     echo "Done with all_tests_under_pycharm.sh - FAILURE"
 fi
+
+# Bring the focus back to the terminal
+echo "Bringing focus back to the original terminal.".
+xdotool windowactivate "$TERMINAL_WIN_ID"
 
 echo "Exiting with code $RETCODE "
 exit $RETCODE
