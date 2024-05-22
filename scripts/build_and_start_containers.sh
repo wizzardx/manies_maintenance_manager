df93f09ee0bf8cfa09b266fb782a10639ca823e5
David <wizzardx@gmail.com>
2024-05-22 18:50:34 +0200

    Remove no-longer needed sudo-requiring tidyup logic from one of the script runner scripts.

--- scripts/build_and_start_containers.sh
+++ scripts/build_and_start_containers.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Remove a directory that can cause some problems with building the image:
-if [ -e .ipython/profile_default ]; then
-    sudo rm -rvf .ipython/profile_default
-fi
-
 # Bring down our containers first before bringing them up. Just in case of weird quirks
 # like eg, some containers unable to shut down for some reason, that I need to check
 # more closely in my dev environment.
