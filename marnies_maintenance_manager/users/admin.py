85df7853e4840ee10e4329da4f1d9d62570472e0
David <wizzardx@gmail.com>
2024-05-31 18:16:21 +0200

    Updates from Django Cookiecutter

--- marnies_maintenance_manager/users/admin.py
+++ marnies_maintenance_manager/users/admin.py
@@ -1,9 +1,9 @@
 """Configure admin panels for user management."""
 
+from allauth.account.decorators import secure_admin_login
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
-from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
 
 from .forms import UserAdminChangeForm
@@ -13,7 +13,8 @@ from .models import User
 if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
     # Force the `admin` sign in process to go through the `django-allauth` workflow:
     # https://docs.allauth.org/en/latest/common/admin.html#admin
-    admin.site.login = login_required(admin.site.login)  # type: ignore[method-assign]
+    admin.autodiscover()
+    admin.site.login = secure_admin_login(admin.site.login)  # type: ignore[method-assign]
 
 
 @admin.register(User)
