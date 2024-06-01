16cbb27a41128d1bec2c284b620441654bbad481
David <wizzardx@gmail.com>
2024-06-01 18:28:02 +0200

    Job model can now be edited in the Admin panel

--- marnies_maintenance_manager/jobs/admin.py
+++ marnies_maintenance_manager/jobs/admin.py
@@ -1,1 +1,7 @@
 """Register models for the jobs application in the Django admin."""
+
+from django.contrib import admin
+
+from marnies_maintenance_manager.jobs.models import Job
+
+admin.site.register(Job)
