7d7700def1eced3600e34d43c666fe536727c48d
David <wizzardx@gmail.com>
2024-06-26 18:47:49 +0200

    Add test coverage

--- marnies_maintenance_manager/jobs/management/commands/setup_manual_dev_testing_data.py
+++ marnies_maintenance_manager/jobs/management/commands/setup_manual_dev_testing_data.py
@@ -1,5 +1,6 @@
 """management.py command to load initial data into the database for manual testing."""
 
+import logging
 from typing import Any
 
 from django.core.management.base import BaseCommand
@@ -7,6 +8,8 @@ from django.db import transaction
 
 from marnies_maintenance_manager.jobs.utils import make_test_user
 from marnies_maintenance_manager.users.models import User
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -32,6 +35,8 @@ class Command(BaseCommand):
             self.stdout.write(
                 self.style.SUCCESS("Successfully created admin user david"),
             )
+        else:
+            logging.debug("Admin user david already exists")
 
     @transaction.atomic
     def create_users_and_data(self) -> None:
@@ -40,6 +45,8 @@ class Command(BaseCommand):
         if not _user_exists("marnie"):
             make_test_user(User, "marnie", is_marnie=True)
             self.stdout.write(self.style.SUCCESS("Successfully created user marnie"))
+        else:
+            logging.debug("User marnie already exists")
 
         # Make 2 Agent users.
         for name in "bob", "alice":
@@ -48,6 +55,8 @@ class Command(BaseCommand):
                 self.stdout.write(
                     self.style.SUCCESS(f"Successfully created user {name}"),
                 )
+            else:
+                logging.debug("User %s already exists", name)
 
             # Over here we can add some records for each user.
 
