54c4dfe8303bf26517d084407f97974cb3d85531
David <wizzardx@gmail.com>
2024-06-28 13:14:01 +0200

    Limit size of uploaded PDF files to 5 MB

--- marnies_maintenance_manager/jobs/tests/test_validators.py
+++ marnies_maintenance_manager/jobs/tests/test_validators.py
@@ -24,3 +24,21 @@ class TestValidatePDFContents:
 
         with pytest.raises(ValidationError, match="The file has already been read"):
             validate_pdf_contents(file)
+
+    @staticmethod
+    def test_should_fail_if_file_too_large(mocker: pytest_mock.MockFixture) -> None:
+        """Test that a ValidationError is raised if the file size is too large.
+
+        Args:
+            mocker (pytest_mock.MockFixture): A pytest-mock fixture.
+        """
+        file = mocker.Mock()
+        file.tell.return_value = 0
+        file.size = 10 * 1024 * 1024
+        file.read.return_value = b"%PDF-1.5"
+
+        with pytest.raises(
+            ValidationError,
+            match="File size should not exceed 5.0 MB.",
+        ):
+            validate_pdf_contents(file)
--- marnies_maintenance_manager/jobs/validators.py
+++ marnies_maintenance_manager/jobs/validators.py
@@ -5,12 +5,15 @@ from django.db.models.fields.files import FieldFile
 from pypdf import PdfReader
 from pypdf.errors import PdfReadError
 
+MAX_PDF_SIZE = 5 * 1024 * 1024  # 5 MB
 
-def validate_pdf_contents(file: FieldFile) -> None:
+
+def validate_pdf_contents(file: FieldFile, max_size: int = MAX_PDF_SIZE) -> None:
     """Validate that the uploaded file is a valid PDF by reading its contents.
 
     Args:
         file (FieldFile): The file to validate.
+        max_size (int): The maximum allowed size of the file in bytes.
 
     Raises:
         ValidationError: If the file is not a valid PDF.
@@ -18,6 +21,12 @@ def validate_pdf_contents(file: FieldFile) -> None:
     if file.tell() != 0:
         msg = "The file has already been read"
         raise ValidationError(msg)
+
+    # Limit the file size:
+    if file.size > max_size:
+        msg = f"File size should not exceed {max_size / (1024 * 1024)} MB."
+        raise ValidationError(msg)
+
     try:  # pylint: disable=too-many-try-statements
         # Create a PdfReader object, to validate for invalid file contents
         PdfReader(file)
